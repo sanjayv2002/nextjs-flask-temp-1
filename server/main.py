66319a3b81f541f6afae7fec3e675ff4d16fde64
SANJAY KUMAR V <70307085+sanjayv2002@users.noreply.github.com>
2023-02-15 07:26:17 +0000

    server det

---
+++ server/main.py
@@ -0,0 +1,16 @@
+from flask import Flask , jsonify
+from flask_cors import cross_origin 
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+@cross_origin()
+def home():
+    return jsonify({"message":"This is the flask template you can work on",["purpose":"This template can be used as server backend which send the JSON responces to the frontend"]})
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True, port = os.getenv("PORT", default = 5000))
+
