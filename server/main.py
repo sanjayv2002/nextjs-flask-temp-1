481de88ac7250ba6b45c7ebc7efe73041a82b969
SANJAY KUMAR V <70307085+sanjayv2002@users.noreply.github.com>
2023-02-16 08:22:03 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 @cross_origin()
 def home():
-    return jsonify({"message":"This is the flask template you can work on",["purpose":"This template can be used as server backend which send the JSON responces to the frontend"]})
+    return jsonify({message:"TEMPLATE READY"})
 
 
-
+#"message":"This is the flask template you can work on",["purpose":"This template can be used as server backend which send the JSON responces to the frontend"
 if __name__ == '__main__':
     app.run(debug = True, port = os.getenv("PORT", default = 5000))
 
